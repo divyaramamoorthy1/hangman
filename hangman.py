bdd75074a51134a36f2f379da40a410dab38fc40
divyaram <divyaram@umich.edu>
2020-08-07 12:01:56 -0700

    function for entering word

---
+++ hangman.py
@@ -0,0 +1,5 @@
+def enter_word():
+    word = raw_input("Welcome to Hangman! Enter the word you want your friend to guess, and then hand your computer over to them: ")
+
+if __name__ == "__main__":
+    enter_word()
